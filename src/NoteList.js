8ef1b8a84ffebab0c7297c7f09f4fb1245c7cc87
wangsongiam <wangsongiam@gmail.com>
2018-11-18 22:34:49 -0800

    added delete to list

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { deleteNote } from './actions.js'
 
 const mapStateToProps = state => ({
   notes: state.notes
 })
 
-const ConnectedList = ({ notes }) => (
-  <ul>
-    {notes.map((note, i) => (
-      <li key={i}>{note.content}</li>
-    ))}
-  </ul>
-)
+const ConnectedList = props => {
+  const clickHandler = (e, id) => {
+    if (e) e.preventDefault()
+    if (e.metaKey) {
+      props.onDeleteNote(id)
+    }
+  }
 
-const NoteList = connect(mapStateToProps)(ConnectedList)
+  return (
+    <ul>
+      {props.notes.map((note, i) => (
+        <li onClick={(e) => clickHandler(e, note.id)} key={i}>{note.content}</li>
+      ))}
+    </ul>
+  )
+}
+
+
+const mapDispatchToProps = dispatch => ({
+  onDeleteNote: (id) => dispatch(deleteNote(id))
+})
+
+const NoteList = connect(mapStateToProps, mapDispatchToProps)(ConnectedList)
 
 export default NoteList
